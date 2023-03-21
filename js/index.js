1da2a3636df89ac1c66971b7104e4f1078c18f77
narasaki ryosei <gennarikun@gmail.com>
2023-03-21 17:43:33 +0900

    update:スムーススクロール実装

--- js/index.js
+++ js/index.js
@@ -7,14 +7,6 @@
     header.classList.toggle('is-showHeader', w.scrollY > 520);
   });
   /**
-    クリックでメニュー表示・非表示
-  */
-  const menu = d.querySelector('.js-menu');
-  const menuIcon = d.querySelector('.js-menu_icon');
-  menuIcon.addEventListener('click', () => {
-    menu.classList.toggle('is-shouMenu');
-  });
-  /**
     スクロールで追従ボタンの表示・非表示
     追従ボタンはpc幅のみ表示
   */
@@ -86,5 +78,65 @@
     } else {
       background.classList.add('is-showbackground');
     }
+    });
+    /**
+      クリックでメニュー表示・非表示
+    */
+    const menu = d.querySelector('.js-menu');
+    const menuIcon = d.querySelector('.js-menu_icon');
+    menuIcon.addEventListener('click', () => {
+      menu.classList.toggle('is-shouMenu');
+    });
+    /**
+      メニューボタンクリック時のスムーススクロール
+    */
+    // イージング関数
+    const Ease = {
+      easeInOut: function (t) { return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; }
+    }
+    // アニメーションの Duration の設定
+    const duration = 500;
+    w.addEventListener('DOMContentLoaded', function () {
+      // スムーススクロールのトリガーを取得
+      const smoothScrollTrigger = d.querySelectorAll('.js-menu_link[href^="#"]');
+      // トリガーをクリックした時に実行
+      for (let i = 0; i < smoothScrollTrigger.length; i++) {
+        smoothScrollTrigger[i].addEventListener('click', (e) => {
+          e.preventDefault();
+          // メニューボタンクリックでメニュー非表示
+          menu.classList.remove('is-shouMenu');
+          // 現在のスクロール位置を取得（クロスブラウザに対応）
+          const currentPostion = d.documentElement.scrollTop || d.body.scrollTop;
+          // スクロール先の要素を取得
+          const href = smoothScrollTrigger[i].getAttribute('href');
+          const targetElement = d.getElementById(href.replace('#', ''));
+          // スクロール先の要素が存在する場合はスムーススクロールを実行
+          if (targetElement) {
+            // スクロール先の要素の位置を取得
+            const targetPosition = w.pageYOffset + targetElement.getBoundingClientRect().top;
+            // スタート時点の時間を取得
+            const startTime = performance.now();
+            // アニメーションのループを定義
+            const loop = function (nowTime) {
+              // スタートからの経過時間を取得
+              const time = nowTime - startTime;
+              // duration を1とした場合の経過時間を計算
+              const normalizedTime = time / duration;
+              // duration に経過時間が達していない場合はアニメーションを実行
+              if (normalizedTime < 1) {
+                // 経過時間とイージングに応じてスクロール位置を変更
+                w.scrollTo(0, currentPostion + ((targetPosition - currentPostion) * Ease.easeInOut(normalizedTime)));
+                // アニメーションを継続
+                requestAnimationFrame(loop);
+              // duration に経過時間が達したら、アニメーションを終了
+              } else {
+                w.scrollTo(0, targetPosition);
+              }
+            }
+            // アニメーションをスタート
+            requestAnimationFrame(loop);
+          }
+        });
+      }
     });
 })(document, window);
